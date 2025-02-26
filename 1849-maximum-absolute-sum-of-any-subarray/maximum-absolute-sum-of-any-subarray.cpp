0a30da3019a6558e806771a855176c8be011d609
T S VENKATA SHANMUKHA THARU <23A91A05J9@aec.edu.in>
2025-02-26 14:42:48 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (51.09%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -3,11 +3,13 @@ public:
     int maxAbsoluteSum(vector<int>& arr) {
         int maxx=0;
         int minn=0;
+        int res=0;
         for(int i=0;i<arr.size();i++)
         {
             maxx=max(0,maxx+arr[i]);
             minn=min(0,minn+arr[i]);
+            res=max(res,maxx-minn);
         }
-        return maxx-minn;
+        return res;
     }
 };
