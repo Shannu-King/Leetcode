01fb8fc25b0187bb2e6d32bdfd655d129ff8c496
T S VENKATA SHANMUKHA THARU <23A91A05J9@aec.edu.in>
2025-03-03 19:02:23 +0530

    Time: 7 ms (59.22%) | Memory: 133.4 MB (61.95%) - LeetSync

---
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int>ans;
+        int c=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]<pivot)
+            ans.push_back(nums[i]);
+            if(nums[i]==pivot)
+            c++;       
+         }
+         while(c--)
+         ans.push_back(pivot);
+         for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]>pivot)
+            ans.push_back(nums[i]);
+        }
+        return ans;
+    }
+};
