7540c943ff759e3d85d7bc394ebe33f74bea71cb
T S VENKATA SHANMUKHA THARU <23A91A05J9@aec.edu.in>
2025-03-05 06:53:28 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (31.17%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        int c=1;
-        long long s=0;
-       for(int i=1;i<n;i++)
-       {
-        s+=2*c;
-        c+=2;
-       }
-       s+=(2*n)-1;
-       return s;
+     return 2*(long )n*(n-1)+1;
     }
 };
