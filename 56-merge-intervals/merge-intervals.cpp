983138d6c3199d1d18916ab6f5ef9db7860840e8
T S VENKATA SHANMUKHA THARU <23A91A05J9@aec.edu.in>
2025-03-24 19:45:46 +0530

    Time: 5 ms (57.92%) | Memory: 23.9 MB (35.39%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>res;
+        sort(intervals.begin(),intervals.end());
+        int first=intervals[0][0];
+        int last=intervals[0][1];
+        for(int i=0;i<intervals.size();i++)
+        {
+            int a=intervals[i][0];
+            int b=intervals[i][1];
+            if(a==last+1||a>last+1)
+            {
+               
+                res.push_back({first,last});
+                first=INT_MAX;
+            }
+          
+            first=min(first,a);
+            last=max(last,b);
+        }
+        res.push_back({first,last});
+        return res;
+    }
+};
