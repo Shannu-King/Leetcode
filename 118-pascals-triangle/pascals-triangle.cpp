b8b0d5348684b6d86bc9d5ebc80af6a3de329618
T S VENKATA SHANMUKHA THARU <23A91A05J9@aec.edu.in>
2025-08-01 09:34:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (88.29%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -5,14 +5,16 @@ public:
         res.push_back({1});
         for(int i=1;i<n;i++)
         {
-            vector<int>row;
-            row.push_back(1);
-          for(int j=0;j<res[i-1].size()-1;j++)
-          {
-            row.push_back(res[i-1][j]+res[i-1][j+1]);
-          }
-          row.push_back(1);
-                res.push_back(row);
+            vector<int>rows;
+            rows.push_back(1);
+            for(int j=0;j<res[i-1].size()-1;j++)
+            {
+                rows.push_back(res[i-1][j]+res[i-1][j+1]);
+            }
+              rows.push_back(1);
+              res.push_back(rows);
+            
+            
         }
         return res;
     }
