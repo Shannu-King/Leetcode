e8c9feac6da0e67abc73c54f75e52fd3a5f8c778
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2025-12-10 18:53:39 +0530

    Time: 50 ms (5.00%) | Memory: 22.6 MB (77.96%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int c=0;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]==nums[i-1])
+            {
+                nums.erase(nums.begin()+i);
+                i--;
+                c++;
+            }
+        }
+        return nums.size();
+    }
+};
