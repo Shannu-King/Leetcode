f6f3f0384b57f731e1d141be7fab5a4d377c7b78
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2025-12-22 21:33:50 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (10.62%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            int req=target-nums[i];
+            //cout<<mp[req]<<" ";
+            if(mp.find(req)!=mp.end())
+            {
+                return {mp[req],i};
+            }
+            mp[nums[i]]=i;
+        }
+        return {};
+    }
+};
