b5d8e5f211e35918de0c6f217cee313906c669b0
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2025-12-23 14:22:52 +0530

    Time: 11 ms (5.24%) | Memory: 11.2 MB (58.97%) - LeetSync

---
+++ 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+        int sum=0;
+        int n=arr.size();
+        for(int i=0;i<arr.size();i++)
+        {
+            sum+=(ceil((n-i)*(i+1)/2.0))*(arr[i]);
+            cout<<sum<<endl;
+        }
+        return sum;
+    }
+};
