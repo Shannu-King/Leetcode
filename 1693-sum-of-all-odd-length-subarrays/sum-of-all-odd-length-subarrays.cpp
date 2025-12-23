54a29982ed6d82544c62f4dd25f4b1bd50bf3c8c
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2025-12-23 14:23:12 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (27.74%) - LeetSync

--- 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.cpp
+++ 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=0;i<arr.size();i++)
         {
             sum+=(ceil((n-i)*(i+1)/2.0))*(arr[i]);
-            cout<<sum<<endl;
+           // cout<<sum<<endl;
         }
         return sum;
     }
