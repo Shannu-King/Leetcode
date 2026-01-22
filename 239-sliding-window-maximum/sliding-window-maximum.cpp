3616eb528b237cba6f9c72ca7a1c08aed1b794f6
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2026-01-22 15:16:14 +0530

    Time: 63 ms (17.16%) | Memory: 153.1 MB (17.19%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n=nums.size();
+        priority_queue<pair<int,int>>pq;
+        vector<int>res;
+        for(int i=0;i<k;i++)
+        {
+            pq.push({nums[i],i});
+           
+        }
+        res.push_back(pq.top().first);
+        for(int i=k;i<n;i++)
+        {
+            pq.push({nums[i],i});
+            while(pq.top().second<i-k+1)
+            pq.pop();
+            res.push_back(pq.top().first);
+        }
+        return res;
+    }
+};
