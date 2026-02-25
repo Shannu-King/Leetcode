d8977acd06097183c46a66724e1e3f37a6275953
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2026-02-25 15:11:40 +0530

    Time: 1184 ms (5.02%) | Memory: 158.1 MB (7.53%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
-       set<string>st;
+        set<string>set;
         if(s.size()<k)
-        return 0;
-        int c=0;
-        for(int i=0;i<k;i++)
-        {
-        
-            string l=s.substr(i,i+k-c);
-
-           // cout<<i<<" "<<i+k<<" ";
-            //cout<<s.substr(i,i+k-c)<<" ";
-            st.insert(l);
-            c++;
+            return false;
+        int n=s.size();
+        for(int i=0;i<k;i++){
+            string l=s.substr(i,k);
+            set.insert(l);
         }
-       
         for(int i=k;i<s.size()-k+1;i++){
-            string l=s.substr(i,i+k-c);
-            //cout<<l<<" ";
-            st.insert(l);
-            c++;
+            string l=s.substr(i,k);
+            set.insert(l);
         }
         int res=pow(2,k);
-       // cout<<res<<" ";
-    //   for(auto i:st)
-    //   cout<<i<<" ";
-        return st.size()>=res;
+        return set.size()>=res;
+        
     }
 };
