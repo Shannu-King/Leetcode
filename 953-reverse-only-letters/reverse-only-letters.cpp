f5129bc453ef092be85ef81df2d87a48b6f61a9f
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2026-04-04 19:18:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (16.34%) - LeetSync

---
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+           int l=0;
+        int r=s.size();
+        while(l<=r)
+        {
+            if(isalpha(s[l])&&isalpha(s[r])){
+            swap(s[l],s[r]);
+            l++;
+            r--;}
+            else if(!isalpha(s[l]))
+            l++;
+            else if(!isalpha(s[r]))
+            r--;
+        }
+        return s;
+    }
+};
