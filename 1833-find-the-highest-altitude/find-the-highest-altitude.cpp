0925013931d2dfe6d537594f90fd6823168e36a4
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2026-06-19 16:15:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (73.87%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int maxx=max(0,gain[0]);
+        for(int i=1;i<gain.size();i++)
+        {
+            gain[i]=gain[i-1]+gain[i];
+            maxx=max(maxx,gain[i]);
+        }
+        return maxx;
+    }
+};
