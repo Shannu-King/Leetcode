c7827fc639201b63dad6c2a3b70a034db476395a
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2026-07-09 11:17:54 +0530

    Time: 3 ms (65.72%) | Memory: 8.2 MB (10.73%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -7,6 +7,12 @@ int fun(int n)
     return fun(n-1)+fun(n-2);
 }
     int fib(int n) {
-        return fun(n);
+        vector<int>dp(n+1,0);
+        if(n==0)
+        return 0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        dp[i]=dp[i-1]+dp[i-2];
+        return dp[n];
     }
 };
