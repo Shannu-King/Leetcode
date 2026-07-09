170189f268ee57d629e74778890458534fbaed7a
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2026-07-09 11:27:01 +0530

    Time: 3 ms (65.72%) | Memory: 8.3 MB (10.73%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-int fun(int n)
+int fun(int n,vector<int>&dp)
 {
     if(n<=1)
     return n;
-    return fun(n-1)+fun(n-2);
+    if(dp[n]!=-1)
+    return dp[n];
+     dp[n]=fun(n-1,dp)+fun(n-2,dp);
+     return dp[n];
 }
     int fib(int n) {
-        vector<int>dp(n+1,0);
+        vector<int>dp(n+1,-1);
         if(n==0)
         return 0;
+        dp[0]=0;
         dp[1]=1;
-        for(int i=2;i<=n;i++)
-        dp[i]=dp[i-1]+dp[i-2];
-        return dp[n];
+        return fun(n,dp);
     }
 };
