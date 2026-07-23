64b2030e8f30d1fd04e387c75b2bd865ee109863
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2026-07-23 11:54:15 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (34.67%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.c
@@ -0,0 +1,24 @@
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+    int i=0,j=0,k=0;
+    int arr[m+n];
+    while(i<m&&j<n){
+        if(nums1[i]< nums2[j])
+        arr[k++]=nums1[i++];
+        else
+        arr[k++]=nums2[j++];
+    }
+    while(i<m)
+    {
+        arr[k++]=nums1[i++];
+    }
+    while(j<n)
+    {
+        arr[k++]=nums2[j++];
+    }
+
+
+for ( i=0;i<(m+n);i++)
+{
+nums1[i]=arr[i];
+}
+}
