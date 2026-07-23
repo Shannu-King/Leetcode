bf97f7992158c5c968249a6ebcbbe28cc45240af
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2026-07-23 12:35:34 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (37.73%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& m) {
+        for(int i=0;i<m.size();i++)
+        {
+            for(int j=i;j<m[0].size();j++)
+            {
+                swap(m[i][j],m[j][i]);
+            }
+            reverse(m[i].begin(),m[i].end());
+        }
+
+    }
+};
