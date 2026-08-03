3fb1cfe7dffc2cb6927120e1858a2851efaf6bd2
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2026-08-03 14:55:59 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (44.04%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         target=target-1;
-        int ans=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+        int ans=lower_bound(nums.begin(),nums.end(),target+1)-nums.begin();
         return ans;
     }
 };
