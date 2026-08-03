b28c177766cbf037d0f137654cda3b8fd553f067
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2026-08-03 22:09:39 +0530

    Time: 10 ms (91.69%) | Memory: 136.3 MB (65.50%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    string stoneGameIII(vector<int>& stoneValue) {
-        int n = stoneValue.size();
+    string stoneGameIII(vector<int>& s) {
+        int n = s.size();
         vector<int> dp(n+1, INT_MIN);
         dp[n] = 0;
 
         for(int i = n-1; i >= 0; i--){
             int take = 0;
             for(int k = 0; k < 3 && i+k < n; k++){
-                take += stoneValue[i+k];
+                take += s[i+k];
                 dp[i] = max(dp[i], take - dp[i+k+1]);
             }
         }
