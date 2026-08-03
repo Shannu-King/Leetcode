3b10bec5ac1c34615a90edc9b3444cfcca7e72cc
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2026-08-03 14:29:47 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (12.99%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
+        int j=0;
         for(int i=0;i<nums.size();i++)
         {
-            if(nums[i]==val)
+            if(nums[i]!=val)
             {
-                nums.erase(nums.begin()+i);
-                i--;
+                nums[j]=nums[i];
+                j++;
             }
         }
-        return nums.size();
+        return j;
     }
 };
