1eab4332596b7f5bc883dce4b026c0a30a7d42ca
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2026-08-07 19:11:02 +0530

    Time: 2 ms (57.32%) | Memory: 36.4 MB (62.59%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-  struct CustomCompare {
-    bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
-        if (a.first == b.first)
-            return a.second > b.second;
-        return a.first > b.first;
-    }
-};
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, CustomCompare> pq;
-  //priority_queue<pair<int, int>, vector<pair<int, int>>, CustomCompare> pq;
-           for(int i=0;i<arr.size();i++)
-           {
-            //    if(arr[i]==x)
-            //    continue;
-               pq.push({abs(x-arr[i]),arr[i]});
-               
-           }
-           vector<int>res;
-           while(!pq.empty()&&k--)
-           {
-               res.push_back(pq.top().second);
-               pq.pop();
-           }
-         sort(res.begin(),res.end());
-         return res;
+        int start=0,end=0;
+        int l=0,r=arr.size()-1;
+        int n=arr.size();
+        while(l<r)
+        {
+            if(r-l+1==k)
+            {
+                start=l;
+                end=r;
+                break;
+            }
+            if(abs(arr[r]-x)>=abs(arr[l]-x))
+            {
+                r--;
+            }
+            else
+            l++;
+        }
+        vector<int>res;
+        for(int i=l;i<=r;i++)
+        res.push_back(arr[i]);
+        return res;
     }
 };
