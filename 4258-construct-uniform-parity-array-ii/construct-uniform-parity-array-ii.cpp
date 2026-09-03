33d3679bfdcd6495478c4a5c79c1d767c3d98f75
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2026-09-03 14:46:08 +0530

    Time: 55 ms (32.20%) | Memory: 166 MB (31.94%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+       
+        sort(nums1.begin(),nums1.end());
+        if(nums1[0]%2!=0)
+        return true;
+        for(int i=0;i<nums1.size();i++)
+        {
+            if(nums1[i]%2==0)
+            continue;
+            else
+            return false;
+
+        }
+        return true;
+    }
+};
