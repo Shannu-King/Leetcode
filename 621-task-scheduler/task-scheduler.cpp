972ed2f079ab30a1f7961debc5b5a227cf54f42a
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2026-09-16 16:14:09 +0530

    Time: 0 ms (100.00%) | Memory: 38.2 MB (83.23%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -8,34 +8,50 @@ public:
         {
             freqMap[tasks[i]]++;
         }
-        for(const auto &it : freqMap)
-        {
-            maxHeap.push(it.second);
-        }
-       int totalTime = 0;
-        while(!maxHeap.empty())
-        {
-            vector<int> temp;
-             int cycleCount = n + 1;
-             while(cycleCount > 0 && ! maxHeap.empty())
-             {
-                int count = maxHeap.top();
-                maxHeap.pop();
-                cycleCount--;
-                totalTime ++;
-                count --;
-                if(count > 0)
-                temp.push_back( count );
 
-             }
+        int maxxCount=0;
+        int maxxFreq=0;
+        for(const auto & it : freqMap)
+        {
             
-             for(const auto &it:temp)
-             maxHeap.push(it);
-              if(!maxHeap.empty())
-             totalTime += cycleCount;
-
-
+            if(it.second > maxxFreq)
+            maxxFreq=it.second;
         }
-        return totalTime;
+         for(const auto & it : freqMap)
+        {
+            
+            if(it.second==maxxFreq)
+           maxxCount ++;
+        }
+    //     for(const auto &it : freqMap)
+    //     {
+    //         maxHeap.push(it.second);
+    //     }
+    //    int totalTime = 0;
+    //     while(!maxHeap.empty())
+    //     {
+    //         vector<int> temp;
+    //          int cycleCount = n + 1;
+    //          while(cycleCount > 0 && ! maxHeap.empty())
+    //          {
+    //             int count = maxHeap.top();
+    //             maxHeap.pop();
+    //             cycleCount--;
+    //             totalTime ++;
+    //             count --;
+    //             if(count > 0)
+    //             temp.push_back( count );
+
+    //          }
+            
+    //          for(const auto &it:temp)
+    //          maxHeap.push(it);
+    //           if(!maxHeap.empty())
+    //          totalTime += cycleCount;
+
+
+    //     }
+
+        return max((maxxFreq-1) * (n + 1) + maxxCount , tasksSize);
     }
 };
