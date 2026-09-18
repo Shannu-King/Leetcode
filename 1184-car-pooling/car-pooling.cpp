0494a20caeb2958ac118fd7e9740d59de080ac98
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2026-09-18 11:14:35 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (69.62%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-         sort(trips.begin(), trips.end(), [](const vector<int>& a, const vector<int>& b) {
-        return a[1] < b[1]; 
-    });
+    //      sort(trips.begin(), trips.end(), [](const vector<int>& a, const vector<int>& b) {
+    //     return a[1] < b[1]; 
+    // });
+    vector<int>change(1001 ,0);
     int tripSize = trips.size();
     for(int i = 0; i < tripSize; i++)
     {
-        int currCapacity = trips[i][0];
-        for(int j = i - 1; j >= 0; j--)
-        {
-            if(trips[i][1] < trips [j][2])
-            currCapacity += trips [j][0];
-        }
+        change[trips[i][1]] += trips[i][0];
+        change[trips[i][2]] -= trips[i][0];
+
+    }
+    int currCapacity = 0;
+    for(int i = 0; i < 1001; i++)
+    {
+        currCapacity += change[i];
         if(currCapacity > capacity)
         return false;
-
     }
     return true;
     }
