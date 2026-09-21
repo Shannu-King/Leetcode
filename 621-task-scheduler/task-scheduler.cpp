2ddb49778a1481d3d495e287ba2f8e8841641db3
Shanmukh Tharun <23A91A05J9@aec.edu.in>
2026-09-21 11:43:57 +0530

    Time: 0 ms (100.00%) | Memory: 38.2 MB (95.45%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        priority_queue<int> maxHeap;
         unordered_map<char, int> freqMap;
         int tasksSize = tasks.size();
         for(int i = 0; i < tasksSize; i++)
@@ -23,35 +22,6 @@ public:
             if(it.second==maxxFreq)
            maxxCount ++;
         }
-    //     for(const auto &it : freqMap)
-    //     {
-    //         maxHeap.push(it.second);
-    //     }
-    //    int totalTime = 0;
-    //     while(!maxHeap.empty())
-    //     {
-    //         vector<int> temp;
-    //          int cycleCount = n + 1;
-    //          while(cycleCount > 0 && ! maxHeap.empty())
-    //          {
-    //             int count = maxHeap.top();
-    //             maxHeap.pop();
-    //             cycleCount--;
-    //             totalTime ++;
-    //             count --;
-    //             if(count > 0)
-    //             temp.push_back( count );
-
-    //          }
-            
-    //          for(const auto &it:temp)
-    //          maxHeap.push(it);
-    //           if(!maxHeap.empty())
-    //          totalTime += cycleCount;
-
-
-    //     }
-
         return max((maxxFreq-1) * (n + 1) + maxxCount , tasksSize);
     }
 };
